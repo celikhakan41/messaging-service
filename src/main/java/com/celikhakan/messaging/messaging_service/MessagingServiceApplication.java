0417b9526d13034912c017b502d1360cb05afebb
celikhakan41 <celikhakan41@yahoo.com>
2025-03-31 00:26:18 +0300

    Add initial configuration for Docker, logging, and basic application structure

--- src/main/java/com/celikhakan/messaging/messaging_service/MessagingServiceApplication.java
+++ src/main/java/com/celikhakan/messaging/messaging_service/MessagingServiceApplication.java
@@ -1,13 +1,20 @@
 package com.celikhakan.messaging.messaging_service;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MessagingServiceApplication {
+	private static final Logger logger = LoggerFactory.getLogger(MessagingServiceApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingServiceApplication.class, args);
+		logger.info("🚀 Hello Elastic from Spring Boot - Hakan");
 	}
 
 }
